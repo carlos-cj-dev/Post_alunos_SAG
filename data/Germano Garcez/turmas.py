e015386a302370e393df5a7a0e80fdc4fa75ea35
carlos-cj-dev <cmj.acad@gmail.com>
2025-05-14 12:40:11 -0300

    update

---
+++ data/Germano Garcez/turmas.py
@@ -0,0 +1,20 @@
+[
+{
+"nome": "1º ANO/TARDE/2025",
+"escola_id": 98,
+"serie": "PRIMEIRO_ANO",
+"turno": "TARDE"
+},
+{
+"nome": "2º ANO/TARDE/2025",
+"escola_id": 98,
+"serie": "SEGUNDO_ANO",
+"turno": "TARDE"
+},
+{
+"nome": "3º ANO/TARDE/2025",
+"escola_id": 98,
+"serie": "TERCEIRO_ANO",
+"turno": "TARDE"
+}
+]
