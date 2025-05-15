018c72a6f9d7553696df9a21f5969385174d9cbb
carlos-cj-dev <cmj.acad@gmail.com>
2025-05-14 23:18:55 -0300

    update school

---
+++ data/JOSÉ MIGUEL DUAILIBE/turmas.py
@@ -0,0 +1,26 @@
+[
+  {
+    "nome": "1º ANO A/MANHÃ/2025",
+    "escola_id": 49,
+    "serie": "PRIMEIRO_ANO",
+    "turno": "MANHA"
+  },
+  {
+    "nome": "2º ANO/MANHÃ/2025",
+    "escola_id": 49,
+    "serie": "SEGUNDO_ANO",
+    "turno": "MANHA"
+  },
+  {
+    "nome": "5º ANO A/MANHÃ/2025",
+    "escola_id": 49,
+    "serie": "QUINTO_ANO",
+    "turno": "MANHA"
+  },
+  {
+    "nome": "5º ANO B/MANHÃ/2025",
+    "escola_id": 49,
+    "serie": "QUINTO_ANO",
+    "turno": "MANHA"
+  }
+]
